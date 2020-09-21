50ebe2cf95dc2421696e23b875abf2b1e36a0154
Quentin Brunet <hello@quentinbrunet.com>
2020-09-22 01:25:56 +0200

    Quick fix

--- app/src/views/WelcomeView.js
+++ app/src/views/WelcomeView.js
@@ -9,11 +9,11 @@ export default () => {
                         <div className="u-mb-lg">Bonjour Quentin !</div>
                         <div className="u-font-size-xl u-mb-lg">Classement</div>
                         <ul className="u-mb-lg">
-                            <li className="u-font-size-base">1. Quentin | 12 wins</li>
-                            <li className="u-font-size-base">2. Marion | 7 wins</li>
-                            <li className="u-font-size-base">3. Alex | 4 wins</li>
-                            <li className="u-font-size-base">4. Greg | 2 wins</li>
-                            <li className="u-font-size-base">5. Noob | 0 win</li>
+                            <li>1. Quentin | 12 wins</li>
+                            <li>2. Marion | 7 wins</li>
+                            <li>3. Alex | 4 wins</li>
+                            <li>4. Greg | 2 wins</li>
+                            <li>5. Noob | 0 win</li>
                         </ul>
                         <button type="submit" onClick={() => console.log('TODO')}>
                             Lancer une partie
